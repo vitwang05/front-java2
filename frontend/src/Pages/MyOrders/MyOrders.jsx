aacbbc7217bcdea59ccf85a91d7d611bde137ffb
PKshift05 <quang0501203@gmail.com>
2025-04-02 15:00:04 +0700

    my order

--- frontend/src/Pages/MyOrders/MyOrders.jsx
+++ frontend/src/Pages/MyOrders/MyOrders.jsx
@@ -10,9 +10,8 @@ const MyOrders = () => {
     const [data,setData] = useState([])
 
     const fetchOrders = async()=>{
-        const response = await axios.post(url+'/api/order/userorders',{},{headers:{token}})
-        setData(response.data.data)
-        
+        const response = await axios.get(url+'order/getAllOrder')
+        setData(response.data.result)
     }
 
     useEffect(()=>{
@@ -20,34 +19,32 @@ const MyOrders = () => {
             fetchOrders()
         }
     },[token])
-
+    console.log(data)
 
     return (
         <div className='my-orders'>
             <h2>My Orders</h2>
             <div className="container">
-                {data.map((order,index)=>{
-                    return(
-                        <div key={index} className='my-orders-order'>
-                            <img src={assets.parcel_icon} alt="" />
-                            <p>{order.items.map((item,index)=>{
-                                if (index === order.items.length-1) {
-                                    return item.name+" x " +item.quantity
-                                }
-                                else{
-                                    return item.name+" x " +item.quantity +", "
-                                }
-                            })}</p>
-                            <p>${order.amount}.00</p>
-                            <p>Items: {order.items.length}</p>
-                            <p><span>&#x25cf;</span><b>{order.status}</b></p>
-                            <button onClick={fetchOrders}>Track Order</button>
-                        </div>
-                    )
-                })}
+                {data.reverse().map((order, index) => (
+                    <div key={index} className='my-orders-order'>
+                        <img src={assets.parcel_icon} alt="" />
+                        <p>
+                            {order.orderItems.map((item, i) => (
+                                i === order.orderItems.length - 1
+                                    ? `${item.foodName} x ${item.quantity}`
+                                    : `${item.foodName} x ${item.quantity}, `
+                            ))}
+                        </p>
+                        <p>${order.amount ? order.amount.toFixed(2) : "N/A"}</p>
+                        <p>Items: {order.orderItems.length}</p>
+                        <p><span>&#x25cf;</span><b>{order.status}</b></p>
+                        <button onClick={fetchOrders}>Track Order</button>
+                    </div>
+                ))}
             </div>
         </div>
-    )
+    );
+    
 }
 
 export default MyOrders
